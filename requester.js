0b4b8103a822154f079f6f8f68324d907abd1e04
rslgp <rslgp@cin.ufpe.br>
2019-10-21 10:31:18 -0300

    fix even more hard to catch

--- requester.js
+++ requester.js
@@ -34,20 +34,24 @@ if (!fs.existsSync(dir)){
 	  
 	  //catch me if you can
 	switch(Math.floor(Math.random() * 3)){
-		case 0: //android
-		options.user-agent = "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Mobile Safari/537.36";
+		 //android
+		case 0:
+		options.headers['user-agent'] = "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Mobile Safari/537.36";
 		break;
 		
-		case 2: //iphone
-		options.user-agent = "Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1";
+		//iphone
+		case 2: 
+		options.headers['user-agent'] = "Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1";
 		break;
 		
-		case 1: //ipad
-		options.user-agent = "Mozilla/5.0 (iPad; CPU OS 11_0 like Mac OS X) AppleWebKit/604.1.34 (KHTML, like Gecko) Version/11.0 Mobile/15A5341f Safari/604.1";
+		//ipad
+		case 1: 
+		options.headers['user-agent'] = "Mozilla/5.0 (iPad; CPU OS 11_0 like Mac OS X) AppleWebKit/604.1.34 (KHTML, like Gecko) Version/11.0 Mobile/15A5341f Safari/604.1";
 		break;
 		
-		default: //pc
-		options.user-agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36";
+		//pc
+		default: 
+		options.headers['user-agent'] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36";
 		break;
 	}
 	  
